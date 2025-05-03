0d69102155d7c36d5a9308f6a35728c4e6417290
michellesijiama <msijiama@gmail.com>
2025-05-03 19:15:51 -0400

    Create emotionColors.js

---
+++ frontend_web/src/emotionColors.js
@@ -0,0 +1,14 @@
+// emotionColors.js
+import * as THREE from 'three';
+
+const emotionColors = {
+  joy: new THREE.Color('#FFD700'),       // bright yellow
+  sadness: new THREE.Color('#1E90FF'),   // blue
+  anger: new THREE.Color('#FF4500'),     // red-orange
+  fear: new THREE.Color('#800080'),      // purple
+  surprise: new THREE.Color('#00CED1'),  // teal
+  neutral: new THREE.Color('#A9A9A9'),   // gray
+  disgust: new THREE.Color('#556B2F'),   // dark olive green
+};
+
+export default emotionColors;
